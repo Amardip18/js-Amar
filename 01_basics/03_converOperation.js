130d96844e243b3a22a7f156a17471134786673c
Amardip18 <amar1947deep@gmail.com>
2024-08-22 19:01:58 +0000

    03_converOperation

---
+++ 01_basics/03_converOperation.js
@@ -0,0 +1,28 @@
+//let score =33
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+l//et valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+
+
+
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLogged);
+
+// 1=> true; 0=>false
+//"" => false
+//"hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
