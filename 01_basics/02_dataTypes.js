81aa815985a41fd219685cb5d6d1a0ebd44254cb
Amardip18 <amar1947deep@gmail.com>
2024-08-22 18:40:25 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js as newer version
+
+//alert(3+3) we are using nodejs ,not browser
+
+////onsole.log("Hitesh")
+
+//let name = "hitesh"
+//let age = 18
+//let isLoggedIn = false
+
+//number => 2 to power 53
+// bigint
+//string =>""
+//n=boolean =>
+   // symbol => unique
+
+//object
+
+console.log(typeof "hitesh);
+   
