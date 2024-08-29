ed53748a4f6d34192c2f0cbb509d50094d3699b9
Amardip18 <amar1947deep@gmail.com>
2024-08-29 11:50:20 +0000

    Objects4

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -18,4 +18,38 @@ const regularUser = {
         }
     }
 }
-console.log(regularUser.fullname.userfullname);
+console.log(regularUser.fullname?.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+//const obj3 = {obj1, obj2}
+
+//const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3= {...obj1, ...obj2}
+console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 2,
+        email: "h@gmail.com"
+    },
+    {
+        id: 3,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser));// access keys of objects
+console.log(Object.values(tinderUser)); // access values of Objects
+console.log(Object.entries(tinderUser)); // retunr in the form of array and as a nested array
+
+console.log(tinderUser.hasOwnProperty(`isLoggedIn`));
