1c1a9986b1257291d1d1c76752bd6c717b7ff8ec
Amardip18 <amar1947deep@gmail.com>
2024-10-10 18:19:41 +0000

    iiiii

---
+++ 10_classes_and_oops/oop.js
@@ -0,0 +1,13 @@
+const user = {
+    username: "Amar", 
+    power:"20",
+    speed:"30",
+
+    getUserDetails: function(){
+        console.log("Got user details from database");
+    }
+
+}
+
+console.log(user.username);
+//console.log(user.getUserDetails());
